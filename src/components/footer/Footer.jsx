d4baafa6be740b26d8dfe01d359b3e3309d79482
Tanvir1909R <trcr719@gmail.com>
2023-04-22 22:42:22 +0600

    updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,40 +8,17 @@ const Footer = () => {
     <footer>
       <div className="container">
         <div className="footer_wrapper">
-          <div className="company_desc">
-            <img src={logo} alt="logo" />
-            <div>
-              <p>
-                hola@https://juicegin.com <br />
-                1300 589 543
-              </p>
-              <a
-                href="https://www.instagram.com/juice__gin/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <BsInstagram />
-              </a>
-            </div>
-          </div>
-          <div className="timer">
-            <div>
-              <p>000</p>
-              <span>Days</span>
-            </div>
-            <div>
-              <p>00</p>
-              <span>hours</span>
-            </div>
-            <div>
-              <p>00</p>
-              <span>min</span>
-            </div>
-            <div>
-              <p>00</p>
-              <span>sec</span>
-            </div>
-          </div>
+          <a
+            href="https://www.instagram.com/juice__gin/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <BsInstagram />
+          </a>
+          <h1>JOIN OUR COMMUNITY</h1>
+          <button>REGISTER FOR REWARDS</button>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit similique reiciendis dolore ipsam suscipit fugiat magni ipsum, ex aut aliquam hic ab exercitationem atque adipisci debitis deleniti. Animi iure aperiam laborum voluptas ex quo quisquam est autem, porro vitae velit.</p>
+          <p>Website by <strong>G</strong></p>
         </div>
       </div>
     </footer>
